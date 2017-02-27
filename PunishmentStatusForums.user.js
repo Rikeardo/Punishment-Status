669805075ae4724ae29033c688988160dc0fc3f6
Rikard <rikardhypixel@gmail.com>
2017-02-27 21:44:00 +0100

    Update PunishmentStatusForums.user.js

--- PunishmentStatusForums.user.js
+++ PunishmentStatusForums.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Punishment status - Forums
-// @version      0.31
+// @version      0.4
 // @description  Check if a player is currently punished from the server, from forums
 // @author       _Rikardo_
 // @match        https://hypixel.net/threads/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
-
-if(document.getElementsByClassName("titleBar")[0].innerHTML.includes("Report Rule Breakers"))
+var key;
+if(document.getElementsByClassName("titleBar")[0].innerHTML.includes("Report Rule Breakers")||document.getElementsByClassName("titleBar")[0].innerHTML.includes("Ban Appeal"))
 {
     var content = $('.messageText').html();
     var arr = content.split('\n');
@@ -15,12 +15,15 @@ if(document.getElementsByClassName("titleBar")[0].innerHTML.includes("Report Rul
     var nameContainer = "";
     while (number <= 3)
     {
-
         var currentCheck = arr[number];
-
-        if(currentCheck.includes("Reason:") === false && currentCheck.includes("reason:") === false && currentCheck.includes("Reason :") === false && currentCheck.includes("reason :") === false && currentCheck.includes("reason -") === false && currentCheck.includes("Reason -") === false && currentCheck.includes("Offence:") === false && currentCheck.includes("offence:") === false && currentCheck.includes("Rank:") === false && currentCheck.includes("rank:") === false)
+        currentCheck = currentCheck.toLowerCase();
+        while(currentCheck.includes(" "))
         {
-            getRemoved = [" ","<b>","</b>","<br>","</br>","<ul>","</ul>","<li>","</li>","In-Game","In-game","Name","name","IGN","rulebreaker","Theruleviolator(s).","ign:","Ign:","typeofhacks","Typeofhacks","in-game",":","-"];
+            currentCheck = currentCheck.replace(" ","");
+        }
+        if(currentCheck.includes("reason:") === false && currentCheck.includes("reason-") === false && currentCheck.includes("offence:") === false && currentCheck.includes("rank:") === false  && currentCheck.includes("screenshotofbanscreen") === false  && currentCheck.includes("whatisthereason") === false && currentCheck.includes("whywereyoubanned?") === false)
+        {
+            getRemoved = ["<b>","</b>","<br>","</br>","<ul>","</ul>","<li>","</li>","ingame","name","rulebreakers","rulebreaker","theruleviolator","ign:","ign-","typeofhacks","in-game","(s)",":","-","*","(",")","."];
             var current_i = 0;
             while(current_i+1 <= getRemoved.length)
             {
@@ -41,7 +44,14 @@ if(document.getElementsByClassName("titleBar")[0].innerHTML.includes("Report Rul
     console.log(nameContainer);
     if(nameContainer.length > 0)
     {
-        document.cookie = "banChecking='"+nameContainer+"END0FN4ME; domain=.hypixel.net;path=/";
+        key = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
+        console.log(key);
+        var now = new Date();
+        var time = now.getTime();
+        time += 10 * 1000;
+        now.setTime(time);
+        document.cookie = "banChecking"+key+"='"+nameContainer+"END0FN4ME"+key+"; expires=" + now.toUTCString() + "; domain=.hypixel.net;path=/";
+
     }
 
 }
@@ -50,59 +60,67 @@ if(document.getElementsByClassName("titleBar")[0].innerHTML.includes("Report Rul
 setInterval(function()
 {
     var cookieAnswer = document.cookie;
-    if(cookieAnswer.includes("banCheckingAnswer="))
+    if(cookieAnswer.includes("banCheckingAnswer"+key+"="))
     {
-        document.cookie = "banCheckingAnswer=; expires=Thu, 01 Jan 1970 00:00:00 UTC;domain=.hypixel.net; path=/;";
+        console.log(cookieAnswer);
+        document.cookie = "banCheckingAnswer"+key+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC;domain=.hypixel.net; path=/;";
 
-        var start = cookieAnswer.indexOf("banCheckingAnswer=");
-        var end = cookieAnswer.lastIndexOf("END0FANSW3R");
-        var info = cookieAnswer.substring(start+19, end);
-        console.log(info);
+        var startAns = cookieAnswer.indexOf("banCheckingAnswer"+key+"=");
+        var endAns = cookieAnswer.lastIndexOf("END0FANSW3R");
+        var info = cookieAnswer.substring(startAns+19+key.toString().length, endAns);
 
-        var endName = info.indexOf("'");
-        var usernameInfo = info.substring(0, endName);
-        if(info.includes("Banned:")||info.includes("Muted:"))
+        if(info.includes("Does not exist"))
         {
-            if(info.includes("Banned:")&& info.includes("Muted:") === false)
-            {
-                var startBan = info.indexOf("Banned:");
-                var endBan = info.lastIndexOf(",");
-                var banReasonInfo = info.substring(startBan+7, endBan);
-                $("<div style='height: 40px; margin: 0px; display: flex; flex-direction: column; justify-content: center; text-align: center; background-color: lightcoral;'>"+usernameInfo+" is currently banned for \""+banReasonInfo+"\".</div>").insertAfter(".pageNavLinkGroup:first");
-            }
-            if(info.includes("Banned:") === false && info.includes("Muted:"))
-            {
-                var startMute = info.indexOf("Muted:");
-                var endMute = info.lastIndexOf("&");
-                var muteReasonInfo = info.substring(startMute+6, endMute);
-                $("<div style='height: 40px; margin: 0px; display: flex; flex-direction: column; justify-content: center; text-align: center; background-color: lightcoral;'>"+usernameInfo+" is currently muted for \""+muteReasonInfo+"\".</div>").insertAfter(".pageNavLinkGroup:first");
-            }
-            if(info.includes("Banned:") && info.includes("Muted:"))
-            {
-                var startMute2 = info.indexOf("Muted:");
-                var endMute2 = info.lastIndexOf("&");
-                var muteReasonInfo2 = info.substring(startMute2+6, endMute2);
-                var startBan2 = info.indexOf("Banned:");
-                var endBan2 = info.lastIndexOf(",");
-                var banReasonInfo2 = info.substring(startBan2+7, endBan2);
-                console.log(banReasonInfo2);
-                $("<div style='height: 40px; margin: 0px; display: flex; flex-direction: column; justify-content: center; text-align: center; background-color: lightcoral;'>"+usernameInfo+" is currently banned for \""+banReasonInfo2+"\" and muted for \""+muteReasonInfo2+"\".</div>").insertAfter(".pageNavLinkGroup:first");
-            }
+            $("<div style='height: 40px; margin: 0px; display: flex; flex-direction: column; justify-content: center; text-align: center; background-color: yellow;'>Could not find a proper name or the name is wrong.</div>").insertAfter(".pageNavLinkGroup:first");
         }
         else
         {
-            $("<div style='height: 40px; margin: 0px; display: flex; flex-direction: column; justify-content: center; text-align: center; background-color: lightgreen;'>"+usernameInfo+" is not punished.</div>").insertAfter(".pageNavLinkGroup:first");
+            console.log(info);
 
+            var endName = info.indexOf("'");
+            var usernameInfo = info.substring(0, endName);
+            if(info.includes("Banned:")||info.includes("Muted:"))
+            {
+                if(info.includes("Banned:")&& info.includes("Muted:") === false)
+                {
+                    var startBan = info.indexOf("Banned:");
+                    var endBan = info.lastIndexOf(",");
+                    var banReasonInfo = info.substring(startBan+7, endBan);
+                    $("<div style='height: 40px; margin: 0px; display: flex; flex-direction: column; justify-content: center; text-align: center; background-color: lightcoral;'>"+usernameInfo+" is currently banned for \""+banReasonInfo+"\".</div>").insertAfter(".pageNavLinkGroup:first");
+                }
+                if(info.includes("Banned:") === false && info.includes("Muted:"))
+                {
+                    var startMute = info.indexOf("Muted:");
+                    var endMute = info.lastIndexOf("&");
+                    var muteReasonInfo = info.substring(startMute+6, endMute);
+                    $("<div style='height: 40px; margin: 0px; display: flex; flex-direction: column; justify-content: center; text-align: center; background-color: lightcoral;'>"+usernameInfo+" is currently muted for \""+muteReasonInfo+"\".</div>").insertAfter(".pageNavLinkGroup:first");
+                }
+                if(info.includes("Banned:") && info.includes("Muted:"))
+                {
+                    var startMute2 = info.indexOf("Muted:");
+                    var endMute2 = info.lastIndexOf("&");
+                    var muteReasonInfo2 = info.substring(startMute2+6, endMute2);
+                    var startBan2 = info.indexOf("Banned:");
+                    var endBan2 = info.lastIndexOf(",");
+                    var banReasonInfo2 = info.substring(startBan2+7, endBan2);
+                    console.log(banReasonInfo2);
+                    $("<div style='height: 40px; margin: 0px; display: flex; flex-direction: column; justify-content: center; text-align: center; background-color: lightcoral;'>"+usernameInfo+" is currently banned for \""+banReasonInfo2+"\" and muted for \""+muteReasonInfo2+"\".</div>").insertAfter(".pageNavLinkGroup:first");
+                }
+            }
+            else
+            {
+                $("<div style='height: 40px; margin: 0px; display: flex; flex-direction: column; justify-content: center; text-align: center; background-color: lightgreen;'>"+usernameInfo+" is not punished.</div>").insertAfter(".pageNavLinkGroup:first");
+            }
         }
 
     }
 }, 200);
 
-var version = 0.31;
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if (request.readyState == XMLHttpRequest.DONE) {
-        var updatedScriptVersion = request.responseText;
+var version = 0.4;
+var forumUpdateRequest = new XMLHttpRequest();
+forumUpdateRequest.onreadystatechange = function() {
+    if (forumUpdateRequest.readyState == XMLHttpRequest.DONE) {
+        var updatedScriptVersion = forumUpdateRequest.responseText;
         if(version < updatedScriptVersion)
         {
             console.log("Update script");
@@ -110,5 +128,5 @@ request.onreadystatechange = function() {
         }
     }
 };
-request.open('GET', 'https://raw.githubusercontent.com/Rikeardo/Punishment-Status/master/ForumVersion.json', true);
-request.send(null);
+forumUpdateRequest.open('GET', 'https://raw.githubusercontent.com/Rikeardo/Punishment-Status/master/ForumVersion.json', true);
+forumUpdateRequest.send(null);
