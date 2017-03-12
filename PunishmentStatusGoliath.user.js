d7cdd3b30266c0fa621905e42e7b268af8c5f210
Rikard <rikardhypixel@gmail.com>
2017-03-12 21:29:53 +0100

    Update PunishmentStatusGoliath.user.js

--- PunishmentStatusGoliath.user.js
+++ PunishmentStatusGoliath.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Punishment status - Goliath
-// @version      0.5
+// @version      0.6
 // @description  Check if a player is currently punished from the server, from forums
 // @author       _Rikardo_
 // @icon         http://i.imgur.com/9gMGDnD.png
@@ -25,31 +25,45 @@ setInterval(function()
             var muteReason = "";
             var banReason = "";
             var banType = "";
+            var byBans = "";
+            var byMutes = "";
+            var dateBan = "";
+            var dateMute = "";
             var bans = document.getElementsByClassName("uk-width-1-4")[1].innerHTML;
             var mutes = document.getElementsByClassName("uk-width-1-4")[3].innerHTML;
+
             if(bans.includes("Current Ban"))
             {
                 var startBanCut = bans.indexOf("<br>");
                 var endBanCut = bans.indexOf("By:");
                 var cutBan = bans.substring(startBanCut+5, endBanCut);
-                console.log(cutBan);
 
                 var endBanReason = cutBan.indexOf("<br>");
 
                 var startTypeBan = cutBan.indexOf("Type:");
                 var endTypeBan = cutBan.lastIndexOf("<br>");
                 var banTypeG = cutBan.substring(startTypeBan+6,endTypeBan).toLowerCase();
-                console.log(banTypeG);
+
+                var dateBans = bans.substring(bans.indexOf("Date:"),bans.length);
+                var endDateBan = dateBans.indexOf("<br><br>");
+                dateBan = "B"+dateBans.substring(0,endDateBan)+"@B ";
+                console.log(dateBan);
 
                 if(banTypeG === "permanent")
                 {
                     banType = "Type:permanently$";
+                    var startByBans = bans.indexOf("By:");
+                    var endByBans = bans.indexOf("<br>\n<br>");
+                    byBans = "B"+bans.substring(startByBans,endByBans)+"£B ";
                 }
                 else if(banTypeG === "temporary")
                 {
                     banType = "Type:temporarily$";
+                    var startByBans2 = bans.indexOf("By:");
+                    var endByBans2 = bans.indexOf("<br>\nExpires:");
+                    byBans = "B"+bans.substring(startByBans2,endByBans2)+"£B ";
                 }
-
+                console.log(byBans);
                 console.log(banType);
                 banReason = "Banned:" + cutBan.substring(8, endBanReason)+",";
 
@@ -63,14 +77,25 @@ setInterval(function()
 
                 var endMuteReason = cutMute.indexOf("<br>");
                 muteReason = "Muted:" + cutMute.substring(8, endMuteReason) + "&";
-
                 console.log(muteReason);
+
+                var dateMutes = mutes.substring(mutes.indexOf("Date:"),mutes.length);
+                var endDateMute = dateMutes.indexOf("<br>");
+                dateMute = "M"+dateMutes.substring(0,endDateMute)+"@M ";
+                console.log(dateMute);
+
+                var startByMutes = mutes.indexOf("By:");
+                var endByMutes = mutes.indexOf("<br>\nExpires:");
+                byMutes = "M"+mutes.substring(startByMutes,endByMutes)+"£M ";
+                console.log(byMutes);
+
+
             }
             var nowG = new Date();
             var timeG = nowG.getTime();
             timeG += 10 * 1000;
             nowG.setTime(timeG);
-            document.cookie = "answerBanChecking"+forumKeyLoaded+"='"+/([A-Za-z0-9_]{1,16})$/.exec($("#columnx > font:first-of-type").text())[1]+ "'"+banReason+muteReason+banType+"END0FANSW3R; expires=" + nowG.toUTCString() +"; domain=.hypixel.net;path=/";
+            document.cookie = "answerBanChecking"+forumKeyLoaded+"='"+/([A-Za-z0-9_]{1,16})$/.exec($("#columnx > font:first-of-type").text())[1]+ "'"+banReason+dateBan+byBans+banType+muteReason+dateMute+byMutes+"END0FANSW3R; expires=" + nowG.toUTCString() +"; domain=.hypixel.net;path=/";
         }
         else
         {
@@ -105,7 +130,7 @@ setInterval(function()
     }
 }, 600);
 
-var version = 0.5;
+var version = 0.6;
 var request = new XMLHttpRequest();
 request.onreadystatechange = function() {
     if (request.readyState == XMLHttpRequest.DONE) {
